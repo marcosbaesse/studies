f1458198f906dfd37556e61c57abc48a1bf5c19e
Marcos Baesse <marcosbaesse@gmail.com>
2020-08-17 20:56:15 -0300

    change publisher path

---
+++ 02 - backend/Message Brokers/RabbitMQ/RabbitMQClient - publisher- c# benchmark/benchmark-src/publisher/SimplePublisher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace publisher
+{
+    public static class SimplePublisher
+    {
+
+        public static IConnection connection = (new ConnectionFactory() { HostName = "localhost" }).CreateConnection();
+
+        public static async Task Publish(string queue, string message)
+        {
+            Console.WriteLine("yielding");
+            // await Task.Yield();
+            // var factory = new ConnectionFactory() { HostName = "localhost" };
+            // Console.WriteLine("conecting...");
+            // var connection = factory.CreateConnection();
+            // Console.WriteLine("connected");
+            var channel = connection.CreateModel();
+            Console.WriteLine("model");
+
+            channel.QueueDeclare(queue: queue,
+                                durable: true,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+            Console.WriteLine("declared");
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                routingKey: queue,
+                                basicProperties: null,
+                                body: body);
+            Console.WriteLine("ok");
+        }
+    }
+}
