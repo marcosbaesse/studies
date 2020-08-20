3948a8801c4c370c3e4568e758dbf0d018c2d6bc
Marcos Baesse <marcosbaesse@gmail.com>
2020-08-19 23:44:04 -0300

    Fix publisher in new example

--- 02 - backend/Message Brokers/RabbitMQ/RabbitMQClient - publisher- c# benchmark/example-deadlock-publishers/publisher/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace publisher
-{
-    public class Class1
-    {
-    }
-}
---
+++ 02 - backend/Message Brokers/RabbitMQ/RabbitMQClient - publisher- c# benchmark/example-deadlock-publishers/publisher/SimplePublisher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace publisher
+{
+    public static class SimplePublisher
+    {
+
+        public static async Task Publish(string queue, string message)
+        {
+            Console.WriteLine("starting...");
+            // await Task.Yield();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            Console.WriteLine("creating connection...");
+            var connection = factory.CreateConnection();
+            Console.WriteLine("creating model...");
+            var channel = connection.CreateModel();
+            Console.WriteLine("declaring queue...");
+
+            channel.QueueDeclare(queue: queue,
+                                durable: true,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+            Console.WriteLine("queue declared");
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                routingKey: queue,
+                                basicProperties: null,
+                                body: body);
+            Console.WriteLine("done");
+        }
+    }
+}
