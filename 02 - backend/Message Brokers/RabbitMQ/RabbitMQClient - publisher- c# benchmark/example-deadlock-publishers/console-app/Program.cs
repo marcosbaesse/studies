0fbaf54025dbc1ff832695c4a6e63e0b93786c53
Marcos Baesse <marcosbaesse@gmail.com>
2020-08-19 23:45:17 -0300

    Add program test

--- 02 - backend/Message Brokers/RabbitMQ/RabbitMQClient - publisher- c# benchmark/example-deadlock-publishers/console-app/Program.cs
+++ 02 - backend/Message Brokers/RabbitMQ/RabbitMQClient - publisher- c# benchmark/example-deadlock-publishers/console-app/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using publisher;
 
 namespace console_app
 {
@@ -6,7 +10,18 @@ namespace console_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Task> tasks = new List<Task>();
+            
+            for (var i = 0; i < 1; i++)
+            for (var j = 0; j < 100; j++)
+            {
+                tasks.Add(Task.Run(() => SimplePublisher.Publish("queue" + i, "Hello World")));
+            }
+
+            Console.Write(tasks.Count);
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("ok");
         }
     }
 }
